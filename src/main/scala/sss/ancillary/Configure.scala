f4ce68a449f2295cd49f1e076f2e9e2cd4772fa7
michal <mpskowron@gmail.com>
2022-06-03 10:16:48 +0200

    feat: changed Configure.config to be lazy

--- src/main/scala/sss/ancillary/Configure.scala
+++ src/main/scala/sss/ancillary/Configure.scala
@@ -5,7 +5,7 @@ import com.typesafe.config.Config
 
 trait Configure {
 
-  implicit val config = ConfigFactory.load()
+  implicit lazy val config: Config = ConfigFactory.load()
   def config(name: String): Config = config.getConfig(name)
 }
 
